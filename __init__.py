8f8a062e1626d252e185d368561bb13b31887a5f
Josh Kalina <jdkalina@gmail.com>
2020-02-13 08:57:09 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,8 @@
+"""
+Authored by Josh Kalina. Note, this is not an officially supported Refinitiv
+library and is supported to showcase the existing capabilities within the REST
+API. It may not explicitly meet the requirements of your business. For enhance-
+ments or specific questions, please contact me at josh.kalina@refinitiv.com
+"""
+
 from datascope.session import *
